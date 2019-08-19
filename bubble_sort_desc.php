aac5d5e0f18fc5b036a27507bd90dc8a1b61c5d1
mell1022 <mell.mmm.1022@gmail.com>
2019-08-19 16:39:07 +0900

    Fixed the code to make it functional for test

--- bubble_sort_desc.php
+++ bubble_sort_desc.php
@@ -1,50 +1,49 @@
 <?php
-
 /*
 Bubble sort in descending order
 バブルソート　降順
 */
 
-// $arr = array(9,8,7,6,5,4,3,2,1,0);
-// $arr = array(0,1,2,3,4,5,6,7,9,8);
-// $arr = array(0,1,2,3);
-$arr = array(10,8,11,2,3,4,5,6,100,7,9,30,600,21,0);
+function bubbleSortDesc($arr){
 
-$cnt = count($arr);
-$the_number_of_arr = $cnt-1;
+    $the_number_of_arr = count($arr)-1;
 
-for($ii = $the_number_of_arr; $ii >= 0; $ii--){
+    for($ii = $the_number_of_arr; $ii >= 0; $ii--){
+        $is_changed = false;
 
-    $is_changed = false;
+        for($i = $the_number_of_arr; $i > $the_number_of_arr - $ii; $i--){
 
-    // for($i = $the_number_of_arr; $i > 0; $i--){ //extra loop
-    for($i = $the_number_of_arr; $i > $the_number_of_arr - $ii; $i--){
+            $current = $i;
+            $next    = $i - 1;
 
-        $current = $i;
-        $next    = $i - 1;
+            if($arr[$next]  < $arr[$current]){
 
-        if($arr[$next]  < $arr[$current]){
+                $tmp = $arr[$current];
+                $arr[$current] = $arr[$next];
+                $arr[$next] = $tmp;
 
-            $tmp = $arr[$current];
-            $arr[$current] = $arr[$next];
-            $arr[$next] = $tmp;
-
-            $is_changed = true;
+                $is_changed = true;
+            }
         }
 
+        //It's done whern it is not chenged anything.
+        if($is_changed === false){
+            // break;
+            return $arr;
+        }
     }
-
-    //It's done whern it is not chenged anything.
-    if($is_changed === false){
-        break;
-    }
-
 }
 
-print_r($arr);
-/*
-$arr = array(10,8,11,2,3,4,5,6,100,7,9,30,600,21,0);
 
+
+// $check_arr = array(10,8,11,2,3,4,5,6,100,7,9,30,600,21,0);
+// $result = bubbleSortDesc($check_arr);
+// print_r($result);
+
+/*
+//$check_arr = array(10,8,11,2,3,4,5,6,100,7,9,30,600,21,0);
+
+Output:
 Array
 (
     [0] => 600
