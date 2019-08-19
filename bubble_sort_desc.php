a2c54eae09b2aa8fd0df91c8ad32b0e6772231ed
mell1022 <mell.mmm.1022@gmail.com>
2019-08-19 11:14:51 +0900

    Fixed code to work correctly

--- bubble_sort_desc.php
+++ bubble_sort_desc.php
@@ -2,24 +2,23 @@
 
 /*
 Bubble sort in descending order
+バブルソート　降順
 */
 
 // $arr = array(9,8,7,6,5,4,3,2,1,0);
 // $arr = array(0,1,2,3,4,5,6,7,9,8);
-// $arr = array(0,1,2,3,4,5,6,7,8,9);
-$arr = array(10,8,11,2,3,4,5,6,100,7,9,30,600,21);
-
+// $arr = array(0,1,2,3);
+$arr = array(10,8,11,2,3,4,5,6,100,7,9,30,600,21,0);
 
 $cnt = count($arr);
+$the_number_of_arr = $cnt-1;
 
-for($ii = $cnt-1; $ii >= 0; $ii--){
-    // echo $ii.":";
+for($ii = $the_number_of_arr; $ii >= 0; $ii--){
 
-    
     $is_changed = false;
 
-    for($i = $ii; $i >= $ii-$i; $i--){
-        // echo $i." ";
+    // for($i = $the_number_of_arr; $i > 0; $i--){ //extra loop
+    for($i = $the_number_of_arr; $i > $the_number_of_arr - $ii; $i--){
 
         $current = $i;
         $next    = $i - 1;
@@ -35,12 +34,6 @@ for($ii = $cnt-1; $ii >= 0; $ii--){
 
     }
 
-    // echo $ii.":";
-    // foreach($arr as $val){
-    //     echo $val." ";
-    // }
-    // echo "<br>";
-
     //It's done whern it is not chenged anything.
     if($is_changed === false){
         break;
@@ -50,22 +43,24 @@ for($ii = $cnt-1; $ii >= 0; $ii--){
 
 print_r($arr);
 /*
+$arr = array(10,8,11,2,3,4,5,6,100,7,9,30,600,21,0);
+
 Array
 (
     [0] => 600
-    [1] => 10
-    [2] => 100
-    [3] => 8
-    [4] => 11
-    [5] => 30
-    [6] => 2
-    [7] => 3
-    [8] => 6
-    [9] => 4
+    [1] => 100
+    [2] => 10
+    [3] => 30
+    [4] => 21
+    [5] => 8
+    [6] => 11
+    [7] => 9
+    [8] => 7
+    [9] => 6
     [10] => 5
-    [11] => 7
-    [12] => 9
-    [13] => 21
+    [11] => 2
+    [12] => 4
+    [13] => 3
 )
 */
 ?>
